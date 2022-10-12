c87ab4e4d268cd44c5903467c63d779cb7aeb40a
nara9709 <worldkr19@gmail.com>
2022-10-12 19:07:22 -0400

    init setting

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function openPage(id) {
+  const containerList = document.querySelectorAll('.contentContainer');
+
+  //Remove an active class If an element has an active class
+  for (let i = 0; i < containerList.length; i++) {
+    if (containerList[i].classList.contains('active')) {
+      containerList[i].classList.remove('active');
+    }
+  }
+
+  //Add active class
+  const idName = document.querySelector(`#${id}`);
+  if (!idName.classList.contains('active')) {
+    idName.classList.add('active');
+  }
+
+  return;
+}
