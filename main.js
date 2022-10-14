222492aeca89c6fc79f0cae89eab236668ae5f74
nara9709 <worldkr19@gmail.com>
2022-10-14 15:25:44 -0400

    complete dialog content

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Open page
 function openPage(id) {
   const containerList = document.querySelectorAll('.contentContainer');
 
@@ -17,4 +18,25 @@ function openPage(id) {
   }
 
   return;
+}
+
+// Handling modal window
+function handleModal(request) {
+  const modalWindow = document.querySelector('#modalWindow');
+  const sectionTitle = document.querySelector('#sec-title');
+  const sectionContent = document.querySelector('#sec-content');
+
+  //   If request is open, open modal window
+  if (request === 'open') {
+    modalWindow.classList.remove('hidden');
+
+    sectionTitle.classList.add('blur');
+    sectionContent.classList.add('blur');
+  } // If request is colose, close modal window
+  else if (request === 'close') {
+    modalWindow.classList.add('hidden');
+
+    sectionTitle.classList.remove('blur');
+    sectionContent.classList.remove('blur');
+  }
 }
